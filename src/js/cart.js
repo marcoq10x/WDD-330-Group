baf68ec35985e280918c41620c6b5a46fb611b5c
anthonb711 <anthonb711@gmail.com>
2024-05-04 19:44:18 -0400

    Fixing the empty cart console error

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,12 +2,13 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  if (!cartItems)
+  if (!cartItems){
     document.querySelector(".product-list").innerHTML = "Cart is empty!";
-
+  } else {
   const cartAry = Object.values(cartItems);
   const htmlItems = cartAry.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  }
 }
 
 function cartItemTemplate(item) {
