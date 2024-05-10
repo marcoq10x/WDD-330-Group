ce8612a842a7d5d069b07e527ba782fbd6a7a32f
anthonb711 <anthonb711@gmail.com>
2024-05-09 22:34:42 -0400

    W03 Team Assignment Complete with strech

--- src/js/product.js
+++ src/js/product.js
@@ -1,37 +1,29 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import {  getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
+import { findProductById } from "./productData.mjs";
 
-function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
-  if (!cartItems) cartItems = {};
 
-  cartItems[product.Id] = product;
-  setLocalStorage("so-cart", cartItems);
-}
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   e.preventDefault(); // Prevents default button click behavior
+//   try {
+//     const product = await findProductById(e.target.dataset.id);
+//     console.log(product);
+//     if (product) {
+//       addProductToCart(product);
+//     } else {
+//       console.error("Product not found");
+//     }
+//   } catch (error) {
+//     console.error("Error adding product to cart:", error);
+//   }
+// }
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  e.preventDefault(); // Prevents default button click behavior
-  try {
-    const product = await findProductById(e.target.dataset.id);
-    if (product) {
-      addProductToCart(product);
-    } else {
-      console.error("Product not found");
-    }
-  } catch (error) {
-    console.error("Error adding product to cart:", error);
-  }
-}
 const productId = getParam("product");
-console.log(findProductById(productId));
 productDetails(productId);
 
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,56 @@
-import { getParam } from "./utils.mjs";
 
- export default function productDetails(productID) {
-  document.getElementById("productName").innerHTML = "Hello from Product details";
- }
+import { findProductById } from "./productData.mjs";
+import { getParam, getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+
+
+ export default async function productDetails(productID) {
+
+  let productData = await findProductById(productID);
+    console.log(`PRODUCT DATA: ${productData}`);
+    renderProductDetails(productData);
+
+  document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+}
+
+ export function addProductToCart(product) {
+  let cartItems = getLocalStorage("so-cart");
+  if (!cartItems) cartItems = {}; 
+
+  cartItems[product.Id] = product;
+  setLocalStorage("so-cart", cartItems);
+}
+
+ function renderProductDetails(productData) {
+  document.getElementById("productName").innerHTML = productData.Brand.Name;
+  document.getElementById("productNameWithoutBrand").innerHTML = productData.NameWithoutBrand;
+  document.getElementById("productImage").src = productData.Image;
+  document.getElementById("productImage").setAttribute("alt", productData.Name);
+  document.getElementById("productFinalPrice").innerHTML = productData.FinalPrice;
+  document.getElementById("productColorName").innerHTML = productData.Colors[0].ColorName;
+  document.getElementById("productColorName").innerHTML = productData.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").insertAdjacentHTML("afterbegin",productData.DescriptionHtmlSimple);
+  document.getElementById("addToCart").dataset.id = productData.Id;
+
+}
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  e.preventDefault(); // Prevents default button click behavior
+  try {
+    const product = await findProductById(e.target.dataset.id);
+    console.log(product);
+    if (product) {
+      addProductToCart(product);
+    } else {
+      console.error("Product not found");
+    }
+  } catch (error) {
+    console.error("Error adding product to cart:", error);
+  }
+}
+
+const productId = getParam("product");
+productDetails(productId);
+
+
