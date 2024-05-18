680019dfae9b9ef278b0de36d36724ebad52a082
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-05-18 12:54:25 -0700

    chore: Remove unused code and update product details by adding animation to the cart icon

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,5 @@
 import {  getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
-import { findProductById } from "./productData.mjs";
-
-
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   e.preventDefault(); // Prevents default button click behavior
-//   try {
-//     const product = await findProductById(e.target.dataset.id);
-//     console.log(product);
-//     if (product) {
-//       addProductToCart(product);
-//     } else {
-//       console.error("Product not found");
-//     }
-//   } catch (error) {
-//     console.error("Error adding product to cart:", error);
-//   }
-// }
 
 const productId = getParam("product");
 productDetails(productId);
-
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
