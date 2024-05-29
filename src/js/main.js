f4ff3a8b3e0458dc1d7553b55c219857e7ff7e9a
anthonb711 <anthonb711@gmail.com>
2024-05-28 23:44:17 -0400

    Add customizable alert to index.html is complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
+import { buildAlerts, getAlerts } from "./alert.mjs";
 import productList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 productList("product-list");
 loadHeaderFooter();
+buildAlerts();
